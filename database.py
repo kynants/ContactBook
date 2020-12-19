e70694c2b02eca278891e10891fb2c36b28b7c98
Kynan Stevenson <kynants@gmail.com>
2020-12-19 05:47:54 -0500

    Added notes of Q&As of the code.

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ connection = sqlite3.connect('contacts.db')
 # Create a cursor
 cursor_var = connection.cursor()
 
-# Create a table
+# CREATE A TABLE
 # cursor_var.execute("""CREATE TABLE contacts (
 #     name text,
 #     address text,
@@ -14,13 +14,18 @@ cursor_var = connection.cursor()
 #     email text
 # )""")
 
-multiple_contacts = [
-    ('John', '123 Main St', '1234567890', 'john@site.com'),
-    ('Bob', '456 2nd St', '0987654321', 'bob@site.com'),
-    ('Sally', '789 3rd St', '5556667777', 'sally@site.com')
-]
+# INSERT MULTIPLE CONTACTS
+# Question: How do I format the phone numbers?
+# Answer: https://stackoverflow.com/questions/75105/what-datatype-should-be-used-for-storing-phone-numbers-in-sql-server-2005
+# multiple_contacts = [
+#     ('John', '123 Main St', '1234567890', 'john@site.com'),
+#     ('Bob', '456 2nd St', '0987654321', 'bob@site.com'),
+#     ('Sally', '789 3rd St', '5556667777', 'sally@site.com')
+# ]
+# connection.executemany("INSERT INTO contacts VALUES (?,?,?,?)", multiple_contacts)
 
-connection.executemany("INSERT INTO contacts VALUES (?,?,?,?)", multiple_contacts)
+# UPDATE BOB'S PHONE NUMBER
+# 
 
 # Commit command
 connection.commit()
